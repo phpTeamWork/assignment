45b4a8e747a9305942435840c797c5cb7a6306c8
jamie <62026804+buttigiegjamie@users.noreply.github.com>
2020-04-15 21:36:11 +0200

    Starting part 2
    Closure of part 1 and commecing part 2.
    
    Basic structure introduced

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en-MT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Francesca Abela, Jamie Buttigieg Vella">
+    <meta name="description" content="CIS1054 Assignment 2019/2020">
+    <title>Francesca and Jamie's Assignment</title>
+</head>
+<body>
+    <h1>Welcome to our assignment</h1>
+    <h2>Authors:</h2>
+    <ul>
+        <li>Abela Francesca ()</li>
+        <li>Buttigieg Vella Jamie (13601H)</li>
+    </ul>
+    <br>
+    <p>Below you can find the relevant links to the main parts of the assignment:</p>
+    <!--links to the relevant parts of the assignment-->
+    <ol>
+        <li><a href="part1/index.php">Part 1</a>
+        <li><a href="part2/index.php">Part 2</a>
+    </ol>
+</body>
+</html>
---
+++ part2/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en-MT">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Francesca Abela, Jamie Buttigieg Vella">
+    <meta name="description" content="CIS1054 Assignment">
+    <title>Part 2</title>
+</head>
+<body>
+    
+</body>
+</html>
