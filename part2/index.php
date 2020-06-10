706d87befae0883b2469acfec40e47e01f3d22a5
fabela01 <62571941+fabela01@users.noreply.github.com>
2020-06-10 17:26:53 +0200

    Ignore
    Github not allowing to pull origin

--- part2/index.php
+++ part2/index.php
@@ -3,7 +3,6 @@
     require_once __DIR__.'/includes/dbh.php';
     require_once __DIR__.'/top_nav.php';
     require_once __DIR__.'/bottom_nav.php';
-
-
+    
     //Render view
     echo $twig->render('index.html', ['menuTypes' => $menuTypes, 'about' => $about]);
